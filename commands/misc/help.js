56837d63140a3b8a1a783c240c9ddb1ca41ea608
Quy <57100447+Axyzes@users.noreply.github.com>
2020-10-07 11:43:21 +0700

    Update help.js

--- commands/misc/help.js
+++ commands/misc/help.js
@@ -2,23 +2,23 @@ const Discord = require('discord.js');
 
 module.exports.run = async (client, message, args) => {
     const prefix = client.prefix;
-    const helpembed = new Discord.MessageEmbed().setThumbnail("https://i.imgur.com/ioBWNNE.png").setTitle("Pitch+")
+    const helpembed = new Discord.MessageEmbed().setAuthor("Wavior", "https://i.imgur.com/xS9EETM.png" , "https://axyzes.tk")
         .setColor('RANDOM')
-        .addField('Commands:', `\`\`\`md\n# play       | Aliases: p
-# skip       | Aliases: next
-# search     | Aliases: yt
-# loop       | Aliases: repeat
-# nowplaying | Aliases: playing, np
-# pause      |
-# resume     | Aliases: unpause
-# queue      |
-# remove     |
-# shuffle    |
-# volume     |
-# stop       | Aliases: leave, disconnect
-# clear      |
-# skipto     |
-# join       |\`\`\``)
+        .addField('Commands:', `\`\`\`md\n<play Aliases="p">
+<skip Aliases="next">
+<search Aliases="yt">
+<loop Aliases="repeat">
+<nowplaying Aliases="playing , np">
+<pause>
+<resume Aliases="unpause">
+<queue>
+<remove>
+<shuffle>
+<volume>
+<stop Aliases="leave , disconnect">
+<clear>
+<skipto>
+<join>\`\`\``)
             .addField('Quick tutorial', '\`\`\`md\n# loop [Song / Queue]\`\`\`')
     message.channel.send(helpembed);
 };
