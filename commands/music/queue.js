7b81c79cdd8cdf3ca5415f40d60c6509a832348e
Micheal <57100447+Axyzes@users.noreply.github.com>
2020-10-11 14:38:40 +0700

    Update queue.js

--- commands/music/queue.js
+++ commands/music/queue.js
@@ -2,16 +2,16 @@ const Discord = require('discord.js');
 
 module.exports.run = async (client, message, args) => {
     const serverQueue = client.queue.get(message.guild.id);
-    const Return = new Discord.MessageEmbed().setColor("RANDOM")
+    const embedreturn = new Discord.MessageEmbed().setColor("RANDOM")
 
-    if (!message.member.voice.channel) return message.channel.send(Return.setDescription(`You aren\'t connected to a voice channel!`));
+    if (!message.member.voice.channel) return message.channel.send(embedreturn.setDescription(`You aren\'t connected to a voice channel!`));
 
     if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) {
-        return message.channel.send(Return.setDescription(`You aren\'t in the same voice channel as me!`));
+        return message.channel.send(embedreturn.setDescription(`You aren\'t in the same voice channel as me!`));
     };
     
-    if(!message.guild.me.voice.channel) return message.channel.send(Return.setDescription(`I'm not connected to a voice channel!`));
-    if(!serverQueue) return message.channel.send(Return.setDescription('There is nothing currently playing.'));
+    if(!message.guild.me.voice.channel) return message.channel.send(embedreturn.setDescription(`I'm not connected to a voice channel!`));
+    if(!serverQueue) return message.channel.send(embedreturn.setDescription('There is nothing currently playing.'));
     const pageBack = "◀️";
     const pageForward = "▶️";
     const num_per_page = 10; 
@@ -22,7 +22,7 @@ module.exports.run = async (client, message, args) => {
     let currentPage = 0;
     let currentListNum = ((currentPage + 1) * num_per_page) - num_per_page;
     let title = `Total songs: ${serverQueue.songs.length}`;
-    let description = `Page ${currentPage+1} of ${num_pages} | Queue loop: ${serverQueue.loop}\n\n${pageContents[currentPage].map((song, index) => 
+    let description = `Page ${currentPage+1} of ${num_pages} | Queue Loop: ${serverQueue.loop}\n\n${pageContents[currentPage].map((song, index) => 
         `${currentListNum+(index+1)}:  [${song.title}](${song.url})`).join('\n')}\n\n`;
     description += `**Now Playing:** [${serverQueue.songs[0].title}](${serverQueue.songs[0].url})`;
     const embeda = new Discord.MessageEmbed().setTitle(title).setColor('RANDOM').setDescription(description);
@@ -51,7 +51,7 @@ module.exports.run = async (client, message, args) => {
         reaction.users.remove(user);
         currentListNum = ((currentPage + 1) * num_per_page) - num_per_page;
         let title = `Total songs: ${serverQueue.songs.length}`;
-        let description = `**Page ${currentPage+1} of ${num_pages} | Queue loop: ${serverQueue.loop}\n\n${pageContents[currentPage].map((song, index) => 
+        let description = `Page ${currentPage+1} of ${num_pages} | Queue Loop: ${serverQueue.loop}\n\n${pageContents[currentPage].map((song, index) => 
             `${currentListNum+(index+1)}:  [${song.title}](${song.url})`).join('\n')}\n\n`;
         description += `**Now Playing:** [${serverQueue.songs[0].title}](${serverQueue.songs[0].url})`;
         embeda.setTitle(title).setDescription(description);
